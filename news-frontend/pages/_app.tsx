1926d08d8a3f5e46098b58e4f5fb79baf48ff7b8
Sudharsan25 <sudharsanskumar0125@gmail.com>
2025-07-10 14:05:39 -0400

    Added Analytics

--- news-frontend/pages/_app.tsx
+++ news-frontend/pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+import { Analytics } from '@vercel/analytics/next';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <Component {...pageProps} />
+       <Analytics />
     </ThemeProvider>
+    
   );
 }
 
